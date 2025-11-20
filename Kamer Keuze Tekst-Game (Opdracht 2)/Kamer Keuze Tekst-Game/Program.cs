9c2aba1e769caf799645fa4d4606fede3a42f793
Azel6B <aslandeleeuw666@gmail.com>
2025-11-20 10:38:13 +0100

    Begin Van Keuze Game

--- Kamer Keuze Tekst-Game (Opdracht 2)/Kamer Keuze Tekst-Game/Program.cs
+++ Kamer Keuze Tekst-Game (Opdracht 2)/Kamer Keuze Tekst-Game/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,28 @@ namespace Kamer_Keuze_Tekst_Game
     internal class Program
     {
         static void Main(string[] args)
+          
         {
+            bool scissor = false;
+            Console.WriteLine("Je wordt wakker in een ziekenhuis, je hebt geen idee waarom je hier bent of wie je bent, zoek de uitgang");
+            Console.WriteLine("Er zit nog een naald in je arm, je trekt hem eruit, Je Kamer heeft 3 deuren.");
+            Console.WriteLine("1 = De deur rechts links van je bed, het lijkt niet op de uitgang  ");
+            Console.WriteLine("2 = De deur dicht bij het voeten eind van je bed.");
+            Console.WriteLine("3 = Je kijkt naar de raam recht van je bed, wil je naar buiten kijken ");
+            string choice1 = Console.ReadLine();
+            if (choice1 == "1")
+            {
+                Console.WriteLine("je kiest voor de deur rechts links van je bed, het lijdt naar een Badkamer");
+            }
+            else if (choice1 == "2")
+            {
+                Console.WriteLine("Je kiest voor de deur aan het eind van je voeten bed, Het leidt naar een lange hal, je ziet losse bedden op de grond liggen en de lichten flikkeren");
+            }
+            else if (choice1 == "3")
+            {
+                Console.WriteLine("Je kiest voor het raam rechts van je bed, je bent op de 3de verdieping en er zijn wat bosjes beneden");
+            }
+            else Console.WriteLine("Er zijn maar 3 deuren in de kamer");
         }
     }
 }
