4b4d4edc315ecfce42549c58dcb5cb6a9d30b068
Azel6B <aslandeleeuw666@gmail.com>
2025-12-11 11:00:24 +0100

    Started Leeftijd Berekenen

---
+++ Leeftijd Berekenen (Opdracht 4)/Leeftijd Berekenen/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Leeftijd_Berekenen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int CurrentYear = DateTime.Now.Year;
+            Console.Write("Geef je geboortejaar in: ");
+            int BirthYear = Convert.ToInt32(Console.ReadLine());
+            int Age = CurrentYear - BirthYear;
+            Console.WriteLine("Je bent " + Age + " jaar oud.");
+            Console.ReadLine();
+            if (Age >= 18)
+            {
+                Console.WriteLine("Je bent meerderjarig, je mag stemmen.");
+            }
+            else
+            {
+                Console.WriteLine("Je bent minderjarig, je mag niet stemmen.");
+            }
+            Console.WriteLine(
+                "Druk op een toets om af te sluiten..."
+            );
+            Console.ReadKey();
+            Console.WriteLine(
+                "Bedankt voor het gebruiken van dit programma!"
+            );
+        }
+    }
+}
