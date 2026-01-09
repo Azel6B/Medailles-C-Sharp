b56d02957c4e6c71e5face92d9ff51e100282887
Azel6B <aslandeleeuw666@gmail.com>
2026-01-09 11:07:02 +0100

    Added Assignment 5
    Added assignment 5 all medals.
    Changed assignment names to make it more organized.

--- [5]Tafels Generator (Opdracht 5)/Program.cs
+++ [5]Tafels Generator (Opdracht 5)/Program.cs
@@ -10,6 +10,24 @@ namespace Tafels_Generator__Opdracht_5_
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i <= 10; i++)
+                Console.WriteLine(i);
+            Console.Clear(); //Brons
+
+            for (int i = 0; i <= 50; i+=5)
+                Console.WriteLine(i);
+            Console.Clear(); //Zilver
+
+            Console.WriteLine("Geef een getal in:");
+            string invoerGetal = Console.ReadLine();
+            Console.WriteLine("En hoe vaak je het wilt vermenigvuldigen");
+            string vermenigvuldigingsGetal = Console.ReadLine();
+            int getal = Convert.ToInt32(invoerGetal);
+            int vermenigvuldig = Convert.ToInt32(vermenigvuldigingsGetal);
+            for (int i = 1; i <= vermenigvuldig; i++)
+                Console.WriteLine("" + getal + " x " + i + " = " + (getal * i));
+            Console.ReadLine(); //Goud
+
         }
     }
 }
